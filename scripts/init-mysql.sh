b0a0277d95d91af49dff489b55560871b3b6c1c8
smiecj <459559238@qq.com>
2022-01-23 11:24:14 +0000

    fix-dockerfile: 修复mysql 下载地址不稳定的问题，下载地址从dev.mysql.com 换成cdn.mysql.com

--- scripts/init-mysql.sh
+++ scripts/init-mysql.sh
@@ -13,27 +13,29 @@ mysql_sub_version="8.0.27-1"
 system_version="el7"
 mysql_home=/home/modules/mysql
 
+mysql_repo_url="https://cdn.mysql.com/Downloads"
+
 mysql_server_rpm_name="mysql-server.rpm"
 mysql_common_rpm_name="mysql-common.rpm"
 mysql_client_rpm_name="mysql-client.rpm"
 mysql_client_plugins_rpm_name="mysql-client-plugins.rpm"
 mysql_libs_rpm_name="mysql-libs.rpm"
 
-mysql_server_rpm_download_link="https://dev.mysql.com/get/Downloads/MySQL-$mysql_version/mysql-community-server-$mysql_sub_version.$system_version.x86_64.rpm"
-mysql_common_rpm_download_link="https://dev.mysql.com/get/Downloads/MySQL-$mysql_version/mysql-community-common-$mysql_sub_version.$system_version.x86_64.rpm"
-mysql_client_rpm_download_link="https://dev.mysql.com/get/Downloads/MySQL-$mysql_version/mysql-community-client-$mysql_sub_version.$system_version.x86_64.rpm"
-mysql_client_plugins_rpm_download_link="https://dev.mysql.com/get/Downloads/MySQL-$mysql_version/mysql-community-client-plugins-$mysql_sub_version.$system_version.x86_64.rpm"
-mysql_libs_rpm_download_link="https://dev.mysql.com/get/Downloads/MySQL-$mysql_version/mysql-community-libs-$mysql_sub_version.$system_version.x86_64.rpm"
+mysql_server_rpm_download_link="$mysql_repo_url/MySQL-$mysql_version/mysql-community-server-$mysql_sub_version.$system_version.x86_64.rpm"
+mysql_common_rpm_download_link="$mysql_repo_url/MySQL-$mysql_version/mysql-community-common-$mysql_sub_version.$system_version.x86_64.rpm"
+mysql_client_rpm_download_link="$mysql_repo_url/MySQL-$mysql_version/mysql-community-client-$mysql_sub_version.$system_version.x86_64.rpm"
+mysql_client_plugins_rpm_download_link="$mysql_repo_url/MySQL-$mysql_version/mysql-community-client-plugins-$mysql_sub_version.$system_version.x86_64.rpm"
+mysql_libs_rpm_download_link="$mysql_repo_url/MySQL-$mysql_version/mysql-community-libs-$mysql_sub_version.$system_version.x86_64.rpm"
 
 if [ "x86_64" == "$system_arch" ]; then
     echo "The system arch is x86_64"
 else
     echo "The system arch is $system_arch"
-    mysql_server_rpm_download_link="https://dev.mysql.com/get/Downloads/MySQL-$mysql_version/mysql-community-server-$mysql_sub_version.$system_version.aarch64.rpm"
-    mysql_common_rpm_download_link="https://dev.mysql.com/get/Downloads/MySQL-$mysql_version/mysql-community-common-$mysql_sub_version.$system_version.aarch64.rpm"
-    mysql_client_rpm_download_link="https://dev.mysql.com/get/Downloads/MySQL-$mysql_version/mysql-community-client-$mysql_sub_version.$system_version.aarch64.rpm"
-    mysql_client_plugins_rpm_download_link="https://dev.mysql.com/get/Downloads/MySQL-$mysql_version/mysql-community-client-plugins-$mysql_sub_version.$system_version.aarch64.rpm"
-    mysql_libs_rpm_download_link="https://dev.mysql.com/get/Downloads/MySQL-$mysql_version/mysql-community-libs-$mysql_sub_version.$system_version.aarch64.rpm"
+    mysql_server_rpm_download_link="$mysql_repo_url/MySQL-$mysql_version/mysql-community-server-$mysql_sub_version.$system_version.aarch64.rpm"
+    mysql_common_rpm_download_link="$mysql_repo_url/MySQL-$mysql_version/mysql-community-common-$mysql_sub_version.$system_version.aarch64.rpm"
+    mysql_client_rpm_download_link="$mysql_repo_url/MySQL-$mysql_version/mysql-community-client-$mysql_sub_version.$system_version.aarch64.rpm"
+    mysql_client_plugins_rpm_download_link="$mysql_repo_url/MySQL-$mysql_version/mysql-community-client-plugins-$mysql_sub_version.$system_version.aarch64.rpm"
+    mysql_libs_rpm_download_link="$mysql_repo_url/MySQL-$mysql_version/mysql-community-libs-$mysql_sub_version.$system_version.aarch64.rpm"
 fi
 
 ## install mysql
