eddbebf6a9b73a09d965701c3508b0da41bfc2ac
smiecj <459559238@qq.com>
2022-01-24 02:39:50 +0000

    fix-storage: mysql start logic move to ENTRYPOINT, otherwise image will build failed

--- scripts/init-mysql.sh
+++ scripts/init-mysql.sh
@@ -66,15 +66,4 @@ rpm -ivh $mysql_server_rpm_name
 #mv /var/lib/mysql/binlog.index $mysql_home/log
 #chown -R mysql:mysql $mysql_home
 
-## start mysql
-service mysqld start
-
-## set mysql default password and default account
-default_mysql_root_password = "root_Test1qaz"
-if [[ "$mysql_version" =~ ^8.* ]]; then
-    mysql -S /var/lib/mysql/mysql/mysql.sock -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$default_mysql_root_password';"
-    service mysqld restart
-else
-fi
-
 ## if need: uninstall mysql
