7ef9e88dd73d7172e36e4adb647a633ac52ae736
smiecj <459559238@qq.com>
2022-02-11 16:16:15 +0800

    fix-dockerfile: move some env var to bashrc to fix source /etc/profile error

--- scripts/init-system.sh
+++ scripts/init-system.sh
@@ -19,10 +19,10 @@ echo "alias ll='ls -l'" >> ~/.bashrc
 echo "alias rm='rm -f'" >> ~/.bashrc
 
 ## profile
-echo "export LC_CTYPE=en_US.UTF-8" >> /etc/profile
-echo "export LC_ALL=en_US.UTF-8" >> /etc/profile
-echo "export BASHRCSOURCED=Y" >> /etc/profile
-echo "export HISTCONTROL=ignoreboth" >> /etc/profile
+echo "export LC_CTYPE=en_US.UTF-8" >> ~/.bashrc
+echo "export LC_ALL=en_US.UTF-8" >> ~/.bashrc
+echo "export BASHRCSOURCED=Y" >> ~/.bashrc
+echo "export HISTCONTROL=ignoreboth" >> ~/.bashrc
 
 ## locale
 localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
