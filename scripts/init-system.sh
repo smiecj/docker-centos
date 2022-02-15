96ee87efd7f58dbb835a34ff860ef88df79fd6e4
smiecj <459559238@qq.com>
2022-02-15 10:52:38 +0800

    feature-dockerfile: 默认 yum 源替换成国内源，加快yum 安装依赖速度

--- scripts/init-system.sh
+++ scripts/init-system.sh
@@ -13,14 +13,32 @@ pushd $home_path
 
 . ./common.sh
 
-## 保留: centos8 需要先解决 yum 安装依赖失败的问题, 暂未验证
+## yum 替换成国内源
 centos_version=`cat /etc/redhat-release | sed 's/.*release //g' | sed 's/ .*//g'`
 if [[ $centos_version =~ 8.* ]]; then
     mv /etc/yum.repos.d/CentOS-Linux-AppStream.repo /etc/yum.repos.d/CentOS-Linux-AppStream.bak_repo
     cp ../components/yum/CentOS-Linux-AppStream.repo /etc/yum.repos.d/CentOS-Linux-AppStream.repo
     mv /etc/yum.repos.d/CentOS-Linux-BaseOS.repo /etc/yum.repos.d/CentOS-Linux-BaseOS.bak_repo
     curl -Lo /etc/yum.repos.d/CentOS-Linux-BaseOS.repo http://mirrors.cloud.tencent.com/repo/centos8_base.repo
+elif [[ $centos_version =~ 7.* ]]; then
+    ### centos7 需要区分不同的操作系统
+    ### 参考: https://blog.csdn.net/smart9527_zc/article/details/84976097
+    system_arch=`uname -p`
+    if [ "x86_64" == "$system_arch" ]; then
+        mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.bak_repo
+        curl -Lo /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+    elif [ "aarch64" == "$system_arch" ]; then
+        mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.bak_repo
+        cp -f ../components/yum/CentOS-7-epel.repo /etc/yum.repos.d/epel.repo
+        cp -f ../components/yum/CentOS-7-Base.repo /etc/yum.repos.d/CentOS-Base.repo
+        cp -f ../components/yum/RPM-GPG-KEY-CentOS-7 /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+        cp -f ../components/yum/RPM-GPG-KEY-CentOS-7-aarch64 /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7-aarch64
+        rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7-aarch64
+        rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+    fi
 fi
+yum clean all
+yum makecache
 
 ## install centos basic tools
 install_basic_tools
