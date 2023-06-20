f50b7d45a45614fd78f63bb9e84d10160cbe3d5d
smiecj <sysusmieli@gmail.com>
2023-06-20 17:37:40 +0800

    fix zsh install

--- Dockerfiles/system/init-system-zsh.sh
+++ Dockerfiles/system/init-system-zsh.sh
@@ -2,7 +2,7 @@
 
 pushd /tmp
 
-curl ${github_url}/smiecj/shell-tools/archive/refs/tags/v1.0.tar.gz
+curl -LO ${github_url}/smiecj/shell-tools/archive/refs/tags/v1.0.tar.gz
 tar -xzvf v1.0.tar.gz
 rm v1.0.tar.gz
 cd shell-tools-1.0
